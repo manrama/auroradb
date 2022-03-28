118b02ac8fb7b063bb88df0a5d914b47e52e69dd
manrama <44764176+manrama@users.noreply.github.com>
2022-03-28 17:26:43 +0530

    Update create-lambda.sh

--- scripts/create-lambda.sh
+++ scripts/create-lambda.sh
@@ -82,7 +82,7 @@ sleep 10
 echo "Creating Lambda function"
 FUNCTION_ARN=$(aws lambda create-function \
   --function-name leaderboard-api \
-  --runtime nodejs10.x \
+  --runtime nodejs14.x \
   --role ${ROLE_ARN} \
   --handler application/handler.handler \
   --timeout 12 \
